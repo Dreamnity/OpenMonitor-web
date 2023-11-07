9a05c4a3d9cab398f5dbd55173fb4a9e5561bf2a
5UP3R_D1N <42496449+superdinmc@users.noreply.github.com>
2023-11-07 17:55:02 +0000

    template

---
+++ src/routes/protected.jsx
@@ -0,0 +1,40 @@
+import { getSession } from "@solid-auth/base";
+import { signOut } from "@solid-auth/base/client";
+import { Show } from "solid-js";
+import { useRouteData } from "solid-start";
+import { createServerData$, redirect } from "solid-start/server";
+import { authOptions } from "~/server/auth";
+export const routeData = () => {
+  return createServerData$(async (_, event) => {
+    const session = await getSession(event.request, authOptions);
+    if (!session) {
+      throw redirect("/");
+    }
+    return session;
+  });
+};
+const Protected = () => {
+  const session = useRouteData();
+  return (
+    <Show when={session()} keyed>
+      {(us) => (
+        <main>
+          <h1>Protected</h1>
+          {us.user?.image ? <img src={us.user?.image} /> : null}
+          <span>Hey there {us.user?.name}! You are signed in!</span>
+          <button
+            onClick={() =>
+              void signOut({
+                redirectTo: "/",
+                redirect: true,
+              })
+            }
+          >
+            Sign Out
+          </button>
+        </main>
+      )}
+    </Show>
+  );
+};
+export default Protected;
